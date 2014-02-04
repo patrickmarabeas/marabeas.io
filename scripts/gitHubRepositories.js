e566f4c838defdf0fade7c898f1a180fa2f1930e
Patrick Marabeas <patrick@pulse-dev.com.au>
2014-02-05 07:23:59 +1100

    Added more project detail

--- scripts/gitHubRepositories.js
+++ scripts/gitHubRepositories.js
@@ -2,78 +2,101 @@ module.factory('GitFactory', ['$http', function( $http ){
 
 	return {
 
-		repositories: [{
+		repositories: [
+//		{
+//			'name': '',
+//			'source': 'github / gist',
+//			'image': 'images/',
+//			'description': '',
+//			'tags': ['',''],
+//			'category': '',
+//
+// 			REPOSITORY DATA API RETURN
+//			'data': '',
+//
+//			COMMITS DATA API RETURN
+//			'commits': '',
+//
+//			ISSUES DATA API RETURN
+//			'issues': '',
+//		},
+		{
 			'name': 'ng-FitText.js',
 			'source': 'github',
-			'image': '',
-			'tags': '',
-			'category': ''
+			'image': 'images/ng-fittext.jpg',
+			'description': 'ng-FitText.js makes font-sizes flexible. Use this AngularJS directive in your fluid or responsive layout to achieve scalable headlines that fill the width of a parent element.',
+			'tags': ['typography','UI','angular','javascript'],
+			'category': 'production'
 		},{
 			'name': 'ng-Lettering.js',
 			'source': 'github',
-			'image': 'images/ng-letteringjs.jpg',
-			'tags': '',
-			'category': ''
+			'image': '',
+			'description': 'ng-Lettering.js injects spans with semantic class names around letters, words or lines - creating a speedy foundation for radical web typography.',
+			'tags': ['typography','UI','span','angular','javascript'],
+			'category': 'production'
 		},{
 			'name': 'ng-Adoption.js',
 			'source': 'github',
 			'image': '',
+			'description': '',
 			'tags': '',
-			'category': ''
+			'category': 'development'
 		},{
 			'name': 'jQuery-FontSpy.js',
 			'source': 'github',
-			'image': '',
+			'image': 'images/jquery-fontspyjs.jpg',
+			'description': '',
 			'tags': '',
-			'category': ''
+			'category': 'production'
 		},{
 			'name': 'angular-swipe-drawer',
 			'source': 'github',
 			'image': '',
+			'description': '',
 			'tags': '',
-			'category': ''
+			'category': 'experiment'
 		},{
 			'name': 'ng-ScrollSpy.js',
 			'source': 'github',
 			'image': '',
+			'description': '',
 			'tags': '',
-			'category': ''
+			'category': 'development'
 		},{
 			'name': 'marabeas.io',
 			'source': 'github',
-			'image': '',
-			'tags': '',
-			'category': ''
+			'image': 'images/marabeasio.jpg',
+			'description': "The site that you're looking at right now! The site serves as a place to link to things I'm involved in, as well as a demonstration itself of what I can achieve with AngularJS and other web technologies.",
+			'tags': ['web','site','angular','js','javascript','design','UI','UX','user','experience','interface','html','less','css'],
+			'category': 'website'
 		},{
 			'name': 'useLESS-Mixins',
 			'source': 'github',
-			'image': '',
+			'image': 'images/useless.jpg',
+			'description': '',
 			'tags': '',
-			'category': ''
+			'category': 'production'
 		},{
 			'name': 'angular-draggable-parallax-drawer',
 			'source': 'github',
 			'image': '',
+			'description': 'An experiment to replicate the parallax drawer found in some native Android applications. This drawer can be dragged around and when released either opens or closes. Parallax effect on the main window.',
 			'tags': '',
-			'category': ''
+			'category': 'experiment'
 		},{
 			'name': 'ng-YouTubeAPI.js',
 			'source': 'github',
 			'image': '',
+			'description': '',
 			'tags': '',
-			'category': ''
+			'category': 'production'
 		},{
 			'name': 'jQuery-YouTube-API-Player',
 			'source': 'github',
 			'image': '',
+			'description': '',
 			'tags': '',
-			'category': ''
-		},{
-			'name': 'angular-touch.js',
-			'source': 'github',
-			'image': '',
-			'tags': '',
-			'category': ''
+			'category': 'production'
 		}],
 
 		getRepositories: function( repository ) {
@@ -124,18 +147,14 @@ module.factory('GitFactory', ['$http', function( $http ){
 }]);
 
 
+
 module.controller('GitController', [ '$scope', 'GitFactory', function( $scope, GitFactory ) {
 
 	GitFactory.getRepositories();
 
 	$scope.sort = 'last_modified';
 	$scope.reverse = true;
-
 	$scope.gits = GitFactory.repositories;
-
-	$scope.commits = GitFactory.repositories.commits;
-
-	$scope.tab = 'commits';
 
 	$scope.setColor = function( state ) {
 
@@ -153,3 +172,5 @@ module.controller('GitController', [ '$scope', 'GitFactory', function( $scope, G
 	};
 
 }]);
+
+
